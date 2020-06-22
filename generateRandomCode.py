39c48dd86b1b3d8667d22bd4c9715de392c4ba75
Adithya Bhaskara <bhaska.adithy11@svvsd.org>
2020-06-22 13:51:15 -0600

    Added mechanism to generate unique codes only, renamed file.

--- generateRandomCode.py
+++ generateRandomCode.py
@@ -6,9 +6,11 @@ Description: The generateRandomCode() function takes in one parameter,
              length, which is set to 10. The function then generates
              a random alphanumeric code of that length and returns it.
 """
-def generateRandomCode(length=10):
+def generateRandomCode(usedCodes, length=10):
 
     availableCharacters = string.digits+string.ascii_letters
-    return ''.join(random.choice(availableCharacters) for i in range(length))
-
-# print(generateRandomCode())
+    with open("usedCodes.txt","a") as usedCodes:
+        code = ''.join(random.choice(availableCharacters) for i in range(length))
+        return code
+usedCodes = []
+usedCodes.append(generateRandomCode(usedCodes))
